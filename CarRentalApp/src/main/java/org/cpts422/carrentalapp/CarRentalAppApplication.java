2089d76d4e23554abd00f998ed9239eda075da53
yevin215 <66778009+yevin215@users.noreply.github.com>
2025-09-22 11:15:56 -0700

    Addition of HomeController to handel web requests

--- CarRentalApp/src/main/java/org/cpts422/carrentalapp/CarRentalAppApplication.java
+++ CarRentalApp/src/main/java/org/cpts422/carrentalapp/CarRentalAppApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CarRentalAppApplication {
+public class CarRentalAppApplication
+{
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         SpringApplication.run(CarRentalAppApplication.class, args);
     }
 
