7db73a5df6f23850fb3f6673bb656bcebe378080
yevin215 <66778009+yevin215@users.noreply.github.com>
2025-09-22 11:50:52 -0700

    Added AppUser and AppUserRepository to store create and store users

--- CarRentalApp/src/main/java/org/cpts422/carrentalapp/model/MembershipType.java
+++ CarRentalApp/src/main/java/org/cpts422/carrentalapp/model/MembershipType.java
@@ -1,3 +1,9 @@
+// Created by : Yevin
+// Created on : Sep 22
+
+// Last Updated by : Yevin
+// Last Updated on : Sep 22
+
 package org.cpts422.carrentalapp.model;
 
 public enum MembershipType
---
+++ CarRentalApp/src/main/java/org/cpts422/carrentalapp/repo/AppUserRepository.java
@@ -0,0 +1,18 @@
+// Created by : Yevin
+// Created on : Sep 22
+
+// Last Updated by : Yevin
+// Last Updated on : Sep 22
+
+package org.cpts422.carrentalapp.repo;
+
+import java.util.Optional;
+import org.cpts422.carrentalapp.model.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AppUserRepository extends JpaRepository<AppUser, Long>
+{
+    Optional<AppUser> findByUsername(String username);
+    boolean existsByUsername(String username);
+    boolean existsByDriversLicenseNumber(String driversLicenseNumber);
+}
