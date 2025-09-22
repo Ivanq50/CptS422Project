f85770758b34c7c6096897c74fd8af6b4533ff9d
yevin215 <66778009+yevin215@users.noreply.github.com>
2025-09-22 11:02:28 -0700

    Set up Car Rental Project using Java-17 and JDK: Temurin-21

---
+++ CarRentalApp/src/main/java/org/cpts422/carrentalapp/CarRentalAppApplication.java
@@ -0,0 +1,13 @@
+package org.cpts422.carrentalapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarRentalAppApplication.class, args);
+    }
+
+}
