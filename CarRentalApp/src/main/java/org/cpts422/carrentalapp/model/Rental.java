75437e942523fac8ae385431fcc3106f465bf6d4
Ivan Quintero <ivanq2802@gmail.com>
2025-09-24 02:30:27 -0700

    Added the Rental entity to take care of storing values relating to the rental of the vehicle. Contains the getters and setters.

---
+++ CarRentalApp/src/main/java/org/cpts422/carrentalapp/model/Rental.java
@@ -0,0 +1,48 @@
+package org.cpts422.carrentalapp.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+public class Rental {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private AppUser user;
+
+    @ManyToOne
+    @JoinColumn(name = "vehicle_id", nullable = false)
+    private Vehicle vehicle;
+
+    @Column(nullable = false)
+    private LocalDate rentalDate;
+
+    @Column(nullable = false)
+    private LocalTime expectedReturnDate;
+
+    @Column(nullable = false)
+    private LocalTime returnDate;
+
+    private LocalDate totalCharge;
+
+
+    // Getter and setter methods
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public AppUser getUser() { return user; }
+    public void setUser(AppUser user) { this.user = user; }
+    public Vehicle getVehicle() { return vehicle; }
+    public void setVehicle(Vehicle vehicle) { this.vehicle = vehicle; }
+    public LocalDate getRentalDate() { return rentalDate; }
+    public void setRentalDate(LocalDate rentalDate) { this.rentalDate = rentalDate; }
+    public LocalTime getExpectedReturnDate() { return expectedReturnDate; }
+    public void setExpectedReturnDate(LocalTime expectedReturnDate) { this.expectedReturnDate = expectedReturnDate; }
+    public LocalTime getReturnDate() { return returnDate; }
+    public void setReturnDate(LocalTime returnDate) { this.returnDate = returnDate; }
+    public LocalDate getTotalCharge() { return totalCharge; }
+    public void setTotalCharge(LocalDate totalCharge) { this.totalCharge = totalCharge; }
+}
